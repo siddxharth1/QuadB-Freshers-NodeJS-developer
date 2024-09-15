af76db750f569f39e8a4cc58ac7b4fec4a56295f
Siddharth Sharma <vsssiddharth@gmail.com>
2024-09-15 15:04:02 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ app.use(cors());
 
 app.use("/api", tickersRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
